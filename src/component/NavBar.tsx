60bd80e15deb84092749cf9a10391df82a2a48c5
derabola7 <chidera.a@example.com>
2023-04-23 14:38:19 +0100

    added useEffect

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import { HiBars2, HiChevronDown } from "react-icons/hi2";
 import { AiOutlineClose } from "react-icons/ai";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Vercel } from "../../public/assets";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +13,21 @@ interface Props {
 }
 export default function NavBar({ size }: Props) {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [fix, setFix ] = useState<boolean>(false);
+ 
+  useEffect(() => {
+    const setFixed = () => {
+      setFix(window.scrollY > 200);
+console.log("window.scrollY");
+    };
+    window.addEventListener("scroll", setFixed);
+    return() => window.removeEventListener("scroll", setFixed);
+  });
   return (
-    <div className="w-full h-[4rem] flex justify-between z-20 bg-transparent backdrop-blur-lg fixed top-0 left-0 items-center px-4 lg:p-2 lg:px-20">
-      <div className="flex w-24 lg:w-30 items-center">
-       {showMenu ? <Image src={Vercel} width={size} height={size} alt="" /> : <Image src={Vercel} width={size} height={size} alt="" />} 
+    <>
+    <div className={`${ fix ? "w-full h-[4rem] flex justify-between z-20 bg-transparent backdrop-blur-lg fixed top-0 left-0 items-center px-4 lg:p-2 lg:px-20" : "w-full h-[4rem] flex justify-between z-20 bg-transparent backdrop-blur-lg fixed top-0 left-0 items-center px-4 lg:p-2 lg:px-20" }`}>
+      <div className="flex w-24 lg:w-30 items-center z-20">
+        {showMenu ? <Image src={Vercel} width={size} height={size} alt="" /> : <Image src={Vercel} width={size} height={size} alt="" />}
       </div>
       <div className="hidden mt-4 lg:mt-1 lg:block items-center">
         <ul
@@ -24,13 +35,13 @@ export default function NavBar({ size }: Props) {
         >
           <li className="m-0 max-w-[30ch] text-sm opacity-70 flex items-center hover:px-5 hover:py-1 rounded-3xl hover:bg-slate-200 hover:opacity-100">
             <Link href="/Genesispass" className="flex gap-1 items-center">
-              Features 
-              <HiChevronDown size="12" /> 
+              Features
+              <HiChevronDown size="12" />
             </Link>
           </li>
           <li className="m-0 max-w-[30ch] text-sm opacity-70 flex items-center bg-transparent hover:opacity-100">
             <Link href="/Services">Docs</Link>
-        </li>
+          </li>
           <li className="m-0 max-w-[30ch] text-sm opacity-70  flex items-center bg-transparent hover:opacity-100 ">
             <Link href="/Project">Templates</Link>
           </li>
@@ -57,42 +68,39 @@ export default function NavBar({ size }: Props) {
           </li>
         </ul>
       </div>
-
       {showMenu ? (
         <div className="flex items-center">
-          <AiOutlineClose  className="block lg:hidden z-20 relative"
-          size={32}
-          onClick={() => setShowMenu(false)} />
+          <AiOutlineClose className="block lg:hidden z-20 relative"
+            size={32}
+            onClick={() => setShowMenu(false)} />
         </div>
-       
+
       ) : (
         <div className="flex items-center">
-        <HiBars2 className="block relative lg:hidden z-20"
-          size={32}
-          onClick={() => setShowMenu(true)} 
-          />
-        </div>  
+          <HiBars2 className="block relative lg:hidden z-20"
+            size={32}
+            onClick={() => setShowMenu(true)} />
+        </div>
       )}
-      <div
-        className={`${
-          showMenu ? "translate-x-0" : "translate-x-[-100%]"
-        } z-10 w-full fixed flex p-4 bg-[#fff] justify-start items-center top-[4.5rem] left-0 transition-transform `}
-      >
-         <ul
-          className={`${inter.className} bg-[#fff] z-10 w-full h-full flex flex-col justify-start items-start p-1`}
+    </div>
+    <div
+      className={`${showMenu ? "translate-y-0" : "translate-y-[-120%]"} z-10 w-full fixed flex py-5 bg-[#fff] justify-start items-center top-[4.5rem] transition-transform overflow-y-visible`}
+    >
+        <ul
+          className={`${inter.className} bg-[#fff] z-10 w-full h-full flex flex-col justify-start items-start px-2`}
         >
           <div className="mb-4 flex flex-col items-center w-full ">
-          <li className="mb-4 p-1.5 text-sm opacity-70 border rounded-md w-full flex justify-center items-center text-[#666666]">
-            <Link href="/Services  ">Login</Link>
-          </li>
-          <li className="m-0 text-sm p-1.5 flex justify-center border items-center opacity-100 w-full rounded-md bg-[#fafafa] text-[#999999]">
-            <Link href="/Project">Sign Up</Link>
-          </li>
+            <li className="mb-4 p-1.5 text-sm opacity-70 border rounded-md w-full flex justify-center items-center text-[#666666]">
+              <Link href="/Services  ">Login</Link>
+            </li>
+            <li className="m-0 text-sm p-1.5 flex justify-center border items-center opacity-100 w-full rounded-md bg-[#000] text-[#FFF]">
+              <Link href="/Project">Sign Up</Link>
+            </li>
           </div>
           <li className="mb-2 py-2 text-md items-center w-full  border-b text-[#000]">
             <Link href="/Genesispass" className="flex justify-between items-center">
-              Features 
-              <HiChevronDown size="16" /> 
+              Features
+              <HiChevronDown size="16" />
             </Link>
           </li>
           <li className="mb-2 py-2 text-md items-center w-full  border-b text-[#000]">
@@ -116,9 +124,8 @@ export default function NavBar({ size }: Props) {
           <li className="mb-2 py-2 text-md items-center w-full  border-b text-[#000]">
             <Link href="/Genesispass">Contact</Link>
           </li>
-        
         </ul>
       </div>
-    </div>
+      </>
   );
 }
